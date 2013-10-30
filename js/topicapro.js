a756fdab22ab4588f61bef630e6f389a805573af
Tamás Nagy <nagy.tomi@gmail.com>
2013-10-30 14:28:50 +0100

    A nevek feldolgozásában volt hiba.

--- js/topicapro.js
+++ js/topicapro.js
@@ -1,15 +1,17 @@
-window.addEvent( 'domready', function () {
-	$$( '.face b, .arc b' ).each( function ( v ) {
-		var nameTag = $( v );
-		var classifiedsLink = new Element( 'a', {
-			href: 'http://hardverapro.hu/tag/' + nameTag.get( 'text' ).trim(),
-			title: 'Apróhirdetései',
-			target: '_blank',
-			html: 'Apróhirdetései'
-		} );
-		classifiedsLink.inject(
-			new Element( 'div' )
-				.inject( nameTag.getParent( 'div' ) )
-		);
-	} );
-} );
+window.addEvent('domready', function () {
+    var nameRegex = /\/tag\/([\w-]+)\.html/i;
+    $$('.face b, .arc b').each(function (nameTag) {
+        if (result = nameRegex.exec(nameTag.getParents('.msg').getChildren('.head p a:nth-child(2)')[0].getProperty('href'))) {
+            var classifiedsLink = new Element('a', {
+                href: 'http://hardverapro.hu/tag/' + result[1].trim(),
+                title: 'Apróhirdetései',
+                target: '_blank',
+                html: 'Apróhirdetései'
+            });
+            classifiedsLink.inject(
+                new Element('div')
+                .inject(nameTag.getParent('div'))
+                );
+        }
+    });
+});
--- js/userpage.js
+++ js/userpage.js
@@ -1,18 +1,19 @@
-window.addEvent('domready', function() {
-	var nickDiv = $$('div.big div.big:first-child');
-	if ( nickDiv && nickDiv.length )
-		nickDiv = nickDiv[0];
-	var text = nickDiv.getElement('h1').clone();
-	text.getChildren().dispose();
-	var link = new Element('a', {
-		href: 'http://hardverapro.hu/tag/' + text.innerText.trim(),
-		title: 'Apróhirdetései',
-		html: 'Apróhirdetései',
-		target: '_blank'
-	});
-	var div = new Element('div', {
-		'style': 'margin: 5px 20px 1px 20px;'
-	});
-	link.inject(div);
-	div.inject(nickDiv);
+window.addEvent('domready', function () {
+    if (result = /\/tag\/([\w-]+)\.html/i.exec(window.location.href)) {
+        var nickDiv = $$('div.big div.big:first-child');
+        if (nickDiv && nickDiv.length) {
+            nickDiv = nickDiv[0];
+        }
+        var link = new Element('a', {
+            href: 'http://hardverapro.hu/tag/' + result[1].trim(),
+            title: 'Apróhirdetései',
+            html: 'Apróhirdetései',
+            target: '_blank'
+        });
+        var div = new Element('div', {
+            'style': 'margin: 5px 20px 1px 20px;'
+        });
+        link.inject(div);
+        div.inject(nickDiv);
+    }
 });
